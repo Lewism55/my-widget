bf009dbbce8405d0adf9b6d9c021b528353ceb72
Michael Lewis <michael.lewis@springbok.ai>
2023-12-19 11:45:12 +0000

    file creation

---
+++ src/Widget.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Content from './Content';
+import Input from './Input';
+
+const Widget = () => {
+    return (
+        <div>
+            <Header />
+            <Content />
+            <Input />
+        </div>
+    );
+};
+
+export default Widget;
